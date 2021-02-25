be80ea09215241a585cd7fd1118098f0c3c6b38d
veja947 <veja947@gmail.com>
2021-02-24 22:14:57 -0500

    !!!important, render person components list based on state person, instead of hard code the list

--- src/App.js
+++ src/App.js
@@ -81,19 +81,9 @@ class App extends Component {
         if (this.state.showPersons) {
             personList = (
                 <div>
-                    <Person
-                        name={this.state.persons[0].name}
-                        age={this.state.persons[0].age}/>
-                    <Person
-                        myClick={() => {this.changePerson("my new name")}}
-                        name={this.state.persons[1].name}
-                        age={this.state.persons[1].age}>
-                        {this.state.description}
-                    </Person>
-                    <Person
-                        editInput={this.editInputHandler}
-                        name={this.state.persons[2].name}
-                        age={this.state.persons[2].age}/>
+                    {this.state.persons.map((person) => {
+                        return <Person name={person.name} age={person.age} />
+                    })}
                 </div>
             );
         }
